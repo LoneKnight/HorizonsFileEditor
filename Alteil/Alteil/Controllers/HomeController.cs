24f51ef2349ffa074b31977b921cc5f731b45bc6
Levente Kiss <knightofthe0@gmail.com>
2014-02-19 20:18:01 +0100

    File Editor
    First draft, all style, no substance

---
+++ Alteil/Alteil/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alteil.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.filelist = 39;
+            ViewBag.cardlist = 100;
+            return View();
+        }
+
+    }
+}
